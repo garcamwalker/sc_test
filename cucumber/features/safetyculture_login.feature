0022225a39b3b46e7becff1066cc543a18b29a0b
gacawa <gc-walker@hotmail.com>
2016-11-19 18:44:25 +0000

    -

---
+++ cucumber/features/safetyculture_login.feature
@@ -0,0 +1,32 @@
+@wip
+Feature: Safety Culture login
+  As a user
+  I want to log into my Safety Culture account
+  So that I can navigate the dashboard
+
+  Background:
+    Given I am on the Safety Culture log in page
+
+  Scenario: User is directed to dashboard on successfully logging in
+    When I enter correct log in details
+    And I select Log in
+    Then I am directed to the Safety Culture dashboard
+#
+#  Scenario: Validation message on log in - missing password
+#    When I enter my email address without a password
+#    And I select Log in
+#    Then I see a password validation message
+#
+#  Scenario: Validation message on log in - missing email
+#    When I enter my password without an email address
+#    And I select Log in
+#    Then I see an email validation message
+#
+#  Scenario: Link to reset password when incorrect log in details provided
+#    When I enter incorrect log in details
+#    And I select Log in
+#    Then I see a link to reset my password
+
+
+
+
---
+++ cucumber/features/step_definitions/login_steps.rb
@@ -0,0 +1,39 @@
+Given(/^I am on the Safety Culture log in page$/) do
+  visit('https://app.safetyculture.io/#/login')
+end
+
+When(/^I enter correct log in details$/) do
+
+end
+
+When(/^I select Log in$/) do
+
+end
+
+Then(/^I am directed to the Safety Culture dashboard$/) do
+
+end
+#
+# When(/^I enter my email address without a password$/) do
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
+#
+# Then(/^I see a password validation message$/) do
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
+#
+# When(/^I enter my password without an email address$/) do
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
+#
+# Then(/^I see an email validation message$/) do
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
+#
+# When(/^I enter incorrect log in details$/) do
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
+#
+# Then(/^I see a link to reset my password$/) do
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
